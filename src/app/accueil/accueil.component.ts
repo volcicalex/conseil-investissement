f971e9c7e436f4661e0fcf916efcca006f5765f1
lecalvmy <42962272+lecalvmy@users.noreply.github.com>
2019-01-08 14:04:20 +0100

    Début accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,15 +1,72 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+
+export interface Section {
+  name: string;
+}
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
+
 export class AccueilComponent implements OnInit {
 
   constructor() { }
+  myControl = new FormControl();
+  options: string[] = ['nom1', 'nom2', 'nom3'];
+  filteredOptions: Observable<string[]>;
+
+  cat1: Section[] = [
+    {
+      name: 'SousCat1',
+    },
+    {
+      name: 'SousCat2',
+    },
+    {
+      name: 'SousCat3'
+    }
+  ];
+  cat2: Section[] = [
+    {
+      name: 'SousCat4'
+    },
+    {
+      name: 'SousCat5'
+    }
+  ];
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
+  
 }
+
+export interface PeriodicElement {
+  name: string;
+  author: string;
+  date: Date;
+  description: string;
+  commentaires: string[];
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {author: "auteur 1", name: 'Conseil', date: new Date('1/1/16'), description: 'description blabal', commentaires: ["cool", "top"]},
+  {author: "auteur 2", name: 'Conseil 2', date: new Date('3/1/16'), description: 'hello hello', commentaires:[]},
+  {author: "auteur 1", name: 'Conseil 3', date: new Date('3/1/16'), description: 'ceci est un conseil', commentaires:["trop nul"]}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,17 @@ import { appRoutes } from './routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 @NgModule({
@@ -35,10 +42,17 @@ import {MatCardModule} from '@angular/material/card';
     RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
     MatFormFieldModule,
+    MatAutocompleteModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
-
+    MatSidenavModule,
+    MatTreeModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
