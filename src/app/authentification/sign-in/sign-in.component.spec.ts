c4b3e866f5e733c5b1c697feec22083652257033
lecalvmy <42962272+lecalvmy@users.noreply.github.com>
2019-01-30 16:40:35 +0100

    Tests unitaires

--- src/app/authentification/sign-in/sign-in.component.spec.ts
+++ src/app/authentification/sign-in/sign-in.component.spec.ts
@@ -3,30 +3,22 @@ import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing'
 import { SignInComponent } from './sign-in.component';
 import { MatCardModule } from '@angular/material/card';
 import { HeaderComponent } from '../../header/header.component';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 import { InjectionToken, NO_ERRORS_SCHEMA } from '@angular/core';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
-  let auth: AuthService;
-  let builder: FormBuilder;
-  let routeur: Router;
-  let toast: ToastrService
-
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SignInComponent, HeaderComponent ],
       imports: [MatCardModule, ReactiveFormsModule,
-        AngularFireModule.initializeApp(environment.firebaseAppConfig),
         ToastrModule.forRoot()],
       providers: [{
         provide: AngularFireAuth,
@@ -45,10 +37,6 @@ describe('SignInComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should be defined', inject([ AuthService ], (service: AuthService) => {
-    expect(service).toBeDefined();
-  }));
 
   it('should create', () => {
     expect(component).toBeDefined();
--- src/app/tests/unit-testing/unit-testing.component.ts
+++ src/app/tests/unit-testing/unit-testing.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { FormBuilder } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { CategorieService } from '../../services/categorie.service';
 
 @Component({
   selector: 'app-unit-testing',
@@ -9,10 +11,16 @@ import { FormBuilder } from '@angular/forms';
 })
 export class UnitTestingComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, 
+              private catService: CategorieService
+    ) { }
 
   ngOnInit() {
-    this.wrongSignIn()
+    this.wrongSignIn();
+    this.canEdit();
+    this.cantEdit();
+    this.canDelete();
+    this.cantDelete();
   }
 
   /* Tests des services d'authentification */
@@ -26,8 +34,118 @@ export class UnitTestingComponent implements OnInit {
     this.authService.signInUser("alexandre@volvic.fr", "alexandre").catch(
       (error) => {
         html_element.innerText = error.message
-        document.getElementById("tests").after(html_element)
+        document.getElementById("connexion").appendChild(html_element)
       }
     )
   }
+
+  
+  /* Tests des services pour les catégories */
+
+  canEdit(){
+    let html_element = document.createElement("p");
+    html_element.id = "canEdit";
+
+    let formBuilder = new FormBuilder();
+    let signupForm = formBuilder.group({
+      nom: 'testUser',
+      prenom: 'testUser',
+      pseudo: 'testUser',
+      email: 'testUser@mail.com',
+      password: 'testUser',
+      isAdmin: true, isAuthor: true
+    });
+    let user = new User(signupForm);
+    if (this.catService.canEdit) {
+      html_element.innerText = "Test validé : admin peut éditer catégories";
+      html_element.style.color = "green";
+      document.getElementById("categories").appendChild(html_element);
+    }
+    else {
+      html_element.innerText = "Test échoué : admin ne peut pas éditer catégories";
+      html_element.style.color = "red";
+      document.getElementById("categories").appendChild(html_element);
+    }
+  }
+
+  cantEdit(){
+    let html_element = document.createElement("p");
+    html_element.id = "canEdit";
+
+    let formBuilder = new FormBuilder();
+    let signupForm = formBuilder.group({
+      nom: 'testUser',
+      prenom: 'testUser',
+      pseudo: 'testUser',
+      email: 'testUser@mail.com',
+      password: 'testUser',
+      isAdmin: false, isAuthor: false
+    });
+    let user = new User(signupForm);
+    if (this.catService.canEdit) {
+      html_element.innerText = "Test validé : lecteur ne peut pas éditer catégories";
+      html_element.style.color = "green";
+      document.getElementById("categories").appendChild(html_element);
+    }
+    else {
+      html_element.innerText = "Test échoué : lecteur peut éditer catégories";
+      html_element.style.color = "red";
+      document.getElementById("categories").appendChild(html_element);
+    }
+  }
+
+  canDelete(){
+    let html_element = document.createElement("p");
+    html_element.id = "canEdit";
+
+    let formBuilder = new FormBuilder();
+    let signupForm = formBuilder.group({
+      nom: 'testUser',
+      prenom: 'testUser',
+      pseudo: 'testUser',
+      email: 'testUser@mail.com',
+      password: 'testUser',
+      isAdmin: true, isAuthor: true
+    });
+    let user = new User(signupForm);
+    if (this.catService.canDelete) {
+      html_element.innerText = "Test validé : admin peut supprimer catégories";
+      html_element.style.color = "green";
+      document.getElementById("categories").appendChild(html_element);
+    }
+    else {
+      html_element.innerText = "Test échoué : admin ne peut pas supprimer catégories";
+      html_element.style.color = "red";
+      document.getElementById("categories").appendChild(html_element);
+    }
+  }
+
+  cantDelete(){
+    let html_element = document.createElement("p");
+    html_element.id = "canEdit";
+
+    let formBuilder = new FormBuilder();
+    let signupForm = formBuilder.group({
+      nom: 'testUser',
+      prenom: 'testUser',
+      pseudo: 'testUser',
+      email: 'testUser@mail.com',
+      password: 'testUser',
+      isAdmin: false, isAuthor: false
+    });
+    let user = new User(signupForm);
+    if (this.catService.canDelete) {
+      html_element.innerText = "Test validé : lecteur ne peut pas supprimer catégories";
+      html_element.style.color = "green";
+      document.getElementById("categories").appendChild(html_element);
+    }
+    else {
+      html_element.innerText = "Test échoué : lecteur peut supprimer catégories";
+      html_element.style.color = "red";
+      document.getElementById("categories").appendChild(html_element);
+    }
+  }
+
+
+
 }
