289f9837b0dc5b5566d263da5b388cbdf1eb3a14
volcicalex <42962399+volcicalex@users.noreply.github.com>
2019-01-30 16:33:36 +0100

    test authService

--- src/app/tests/unit-testing/unit-testing.component.ts
+++ src/app/tests/unit-testing/unit-testing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { FormBuilder } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-unit-testing',
@@ -12,21 +13,70 @@ export class UnitTestingComponent implements OnInit {
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.rightSignIn()
     this.wrongSignIn()
+    this.wrongCreateUser()
   }
 
   /* Tests des services d'authentification */
   rightSignIn(){
-
+    let html_element = document.createElement("p")
+    html_element.id = "rightSignIn";
+    this.authService.signInUser("unit@testing.com", "unittesting").then(
+      () => {
+        html_element.innerText = "Test reussi"
+        html_element.style.color = "green"
+        document.getElementById("tests").appendChild(html_element)
+      },
+      (error) => {
+        html_element.innerText = "Test échoué: " + error.message
+        html_element.style.color = "red"
+        document.getElementById("tests").appendChild(html_element)
+      }
+    )
   }
 
   wrongSignIn(){
     let html_element = document.createElement("p")
     html_element.id = "wrongSignIn";
-    this.authService.signInUser("alexandre@volvic.fr", "alexandre").catch(
+    this.authService.signInUser("alexandre@volvic.fr", "alexandre").then(
+      () => {
+        html_element.innerText = "Test échoué"
+        html_element.style.color = "red"
+        document.getElementById("tests").appendChild(html_element)
+      },
       (error) => {
-        html_element.innerText = error.message
-        document.getElementById("tests").after(html_element)
+        html_element.innerText = "Test reussi: " + error.message
+        html_element.style.color = "green"
+        document.getElementById("tests").appendChild(html_element)
+      }
+    )
+  }
+
+  wrongCreateUser(){
+    let formBuilder = new FormBuilder();
+    let signupForm = formBuilder.group({
+      nom: "Volcic",
+      prenom: "Alexandre",
+      pseudo: "volcica",
+      email: "unit@testing.com",
+      password: "volcica",
+      isAdmin: true, isAuthor: true
+    });
+    let user = new User(signupForm)
+
+    let html_element = document.createElement("p")
+    html_element.id = "rightCreateUser";
+    this.authService.createNewUser(user, signupForm.value.password).then(
+      () => {
+        html_element.innerText = "Test echoué"
+        html_element.style.color = "red"
+        document.getElementById("tests").appendChild(html_element)
+      },
+      (error) => {
+        html_element.innerText = "Test reussi: " + error.message
+        html_element.style.color = "green"
+        document.getElementById("tests").appendChild(html_element)
       }
     )
   }
